a6237d409215e5c2aa48cfb484666fbdf2a37828
IanOSilva763 <ian.o.s@hotmail.com>
2022-09-27 20:13:53 -0300

    arquivos estaticos

--- sca/wsgi.py
+++ sca/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sca.settings')
 
